6bfc6508449f39bd231313e741a314233c4e8887
T4ras123 <vovatara123@gmail.com>
2024-12-10 17:01:21 +0400

    feat: add NANOGPT_SCALE_INIT for weight initialization in SelfAttention and MLP layers

--- GPT/train.py
+++ GPT/train.py
@@ -27,6 +27,7 @@ class SelfAttention(nn.Module):
         assert config.n_embd % config.n_head == 0
         self.c_attn = nn.Linear(config.n_embd, config.n_embd*3)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1.0
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
@@ -65,6 +66,7 @@ class MLP(nn.Module):
         super().__init__()
         self.c_fc = nn.Linear(config.n_embd, config.n_embd*4)
         self.c_proj = nn.Linear(config.n_embd*4, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1.0
         self.gelu = nn.GELU()
         
     def forward(self, x):
@@ -85,6 +87,20 @@ class GPT(nn.Module):
 
         self.transformer.wte.weight = self.lm_head.weight
 
+        self.apply(self._init_weights)
+
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2*self.config.n_layer)**-0.5
+            torch.nn.init.normal_(module.weight, mean=0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0, std=std)
+            
     def forward(self, idx, targets=None):
         B, T = idx.size()
         assert T <= self.config.block_size 
@@ -145,13 +161,14 @@ class DataLoaderLite:
         return x, y 
         
 if __name__ == "__main__":
-    
+
     from torch.amp import autocast
     from torch.amp import GradScaler
-    
+
+    scaler = GradScaler("cuda")
+
     config = GPTConfig()
     model = GPT(config).to(device)
-    scaler = GradScaler("cuda")
     train_loader = DataLoaderLite(config.batch_size, config.block_size)
 
     model_dict_path = os.path.join(os.path.dirname(__file__), "model.ptl")
