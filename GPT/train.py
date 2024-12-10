fbb1ddd6c6822e4220617aea8fa31109ca5ff674
T4ras123 <vovatara123@gmail.com>
2024-12-10 17:36:49 +0400

    chore: change hyperparameters

--- GPT/train.py
+++ GPT/train.py
@@ -91,8 +91,8 @@ class GPT(nn.Module):
 
 
     def _init_weights(self, module):
+        std = 0.02
         if isinstance(module, nn.Linear):
-            std = 0.02
             if hasattr(module, 'NANOGPT_SCALE_INIT'):
                 std *= (2*self.config.n_layer)**-0.5
             torch.nn.init.normal_(module.weight, mean=0, std=std)
